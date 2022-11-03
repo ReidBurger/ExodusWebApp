4bd23bcbfe90bc6a1f8ae7d9388c55c6d9205e61
ReidBurger <rb1504@messiah.edu>
2022-11-03 15:46:15 -0400

    Ready Set Go

--- score.js
+++ score.js
@@ -6,6 +6,36 @@ function calculateScore() {
     let ecoScore = parseInt(localStorage.getItem("economy"));
     let defScore = parseInt(localStorage.getItem("defense"));
 
+    let station1_nation_score = parseInt(localStorage.getItem("station1_nation_score"));
+    let station2_nation_score = parseInt(localStorage.getItem("station2_nation_score"));
+    let station3_nation_score = parseInt(localStorage.getItem("station3_nation_score"));
+    let station4_nation_score = parseInt(localStorage.getItem("station4_nation_score"));
+    let station5_nation_score = parseInt(localStorage.getItem("station5_nation_score"));
+    let station6_nation_score = parseInt(localStorage.getItem("station6_nation_score"));
+    let station7_nation_score = parseInt(localStorage.getItem("station7_nation_score"));
+    let station8_nation_score = parseInt(localStorage.getItem("station8_nation_score"));
+    let station9_nation_score = parseInt(localStorage.getItem("station9_nation_score"));
+    let station10_nation_score = parseInt(localStorage.getItem("station10_nation_score"));
+    let station11_nation_score = parseInt(localStorage.getItem("station11_nation_score"));
+    let station12_nation_score = parseInt(localStorage.getItem("station12_nation_score"));
+    let station13_nation_score = parseInt(localStorage.getItem("station13_nation_score"));
+    let station14_nation_score = parseInt(localStorage.getItem("station14_nation_score"));
+
+    let station1_planet_score = parseInt(localStorage.getItem("station1_planet_score"));
+    let station2_planet_score = parseInt(localStorage.getItem("station2_planet_score"));
+    let station3_planet_score = parseInt(localStorage.getItem("station3_planet_score"));
+    let station4_planet_score = parseInt(localStorage.getItem("station4_planet_score"));
+    let station5_planet_score = parseInt(localStorage.getItem("station5_planet_score"));
+    let station6_planet_score = parseInt(localStorage.getItem("station6_planet_score"));
+    let station7_planet_score = parseInt(localStorage.getItem("station7_planet_score"));
+    let station8_planet_score = parseInt(localStorage.getItem("station8_planet_score"));
+    let station9_planet_score = parseInt(localStorage.getItem("station9_planet_score"));
+    let station10_planet_score = parseInt(localStorage.getItem("station10_planet_score"));
+    let station11_planet_score = parseInt(localStorage.getItem("station11_planet_score"));
+    let station12_planet_score = parseInt(localStorage.getItem("station12_planet_score"));
+    let station13_planet_score = parseInt(localStorage.getItem("station13_planet_score"));
+    let station14_planet_score = parseInt(localStorage.getItem("station14_planet_score"));
+
     // calculations for froichua
     if (localStorage.getItem("nation") == "froichua") {
         // froichua station 1 options
